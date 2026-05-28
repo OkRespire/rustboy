649fe3d362eec543bf3f079fbc00037881834f89
OkRespire <148709418+OkRespire@users.noreply.github.com.>
2026-05-28 18:54:59 +0100

    added all x = 0 y = 0..7 and z = 0 instructions ( i think)

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,13 @@
 use crate::{memory::Memory, registers::Registers};
 
+enum Condition {
+    NZ,
+    Z,
+    NC,
+    C,
+    None,
+}
+
 #[allow(dead_code)]
 pub struct Cpu {
     registers: Registers,
@@ -27,10 +35,100 @@ impl Cpu {
         op
     }
 
-    pub fn decode(&self, op: u8) {
+    pub fn decode(&mut self, op: u8) {
         let x = (op >> 6) & 0x03;
         let y = (op >> 3) & 0x07;
         let z = op & 0x07;
-        todo!()
+
+        let p = y << 1;
+        let q = y % 2;
+
+        match x {
+            0 => match y {
+                0 => match z {
+                    0 => {}
+                    1 => {}
+                    2 => todo!(),
+                    3 => todo!(),
+                    4 => todo!(),
+                    5 => todo!(),
+                    6 => todo!(),
+                    7 => todo!(),
+                    _ => unreachable!("fail"),
+                },
+                1 => match z {
+                    0 => {
+                        let high = self.fetch();
+                        let low = self.fetch();
+                        let nn: u16 = ((high as u16) << 8) | low as u16;
+                        self.sp = nn;
+                    }
+                    _ => todo!(),
+                },
+                2 => match z {
+                    0 => {
+                        self.fetch();
+                    }
+                    _ => todo!(),
+                },
+                3 => match z {
+                    0 => {
+                        let n = self.fetch() as i8;
+                        self.jr(Condition::None, n);
+                    }
+                    _ => todo!(),
+                },
+                4 => match z {
+                    0 => {
+                        let n = self.fetch() as i8;
+                        self.jr(Condition::NZ, n)
+                    }
+                    _ => todo!(),
+                },
+
+                5 => match z {
+                    0 => {
+                        let n = self.fetch() as i8;
+                        self.jr(Condition::Z, n)
+                    }
+                    _ => todo!(),
+                },
+
+                6 => match z {
+                    0 => {
+                        let n = self.fetch() as i8;
+                        self.jr(Condition::NC, n)
+                    }
+                    _ => todo!(),
+                },
+
+                7 => match z {
+                    0 => {
+                        let n = self.fetch() as i8;
+                        self.jr(Condition::C, n)
+                    }
+                    _ => todo!(),
+                },
+
+                _ => unreachable!("fail"),
+            },
+            1 => todo!(),
+            2 => todo!(),
+            3 => todo!(),
+            _ => unreachable!("opcode unusable"),
+        }
+    }
+
+    fn jr(&mut self, cc: Condition, n: i8) {
+        let should_jump = match cc {
+            Condition::NZ => !self.registers.f.zero,
+            Condition::Z => self.registers.f.zero,
+            Condition::NC => !self.registers.f.carry,
+            Condition::C => self.registers.f.carry,
+            Condition::None => true,
+        };
+        if should_jump {
+            self.pc = self.pc.wrapping_add_signed(n as i16);
+        }
     }
 }
--- src/registers.rs
+++ src/registers.rs
@@ -1,20 +1,60 @@
+/// Adapted from https://rylev.github.io/DMG-01/public/book/cpu/registers.html
+
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+
+#[allow(dead_code)]
+pub struct FlagsRegister {
+    pub zero: bool,
+    pub subtract: bool,
+    pub half_carry: bool,
+    pub carry: bool,
+}
+
+impl From<u8> for FlagsRegister {
+    fn from(byte: u8) -> Self {
+        let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let subtract = ((byte >> SUBTRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+
+        Self {
+            zero,
+            subtract,
+            half_carry,
+            carry,
+        }
+    }
+}
+
+impl From<FlagsRegister> for u8 {
+    fn from(flag: FlagsRegister) -> Self {
+        (if flag.zero { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSITION
+            | (if flag.subtract { 1 } else { 0 }) << SUBTRACT_FLAG_BYTE_POSITION
+            | (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION
+            | (if flag.carry { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
+    }
+}
+
 #[allow(dead_code)]
 pub struct Registers {
-    a: u8,
-    f: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    h: u8,
-    l: u8,
+    pub a: u8,
+    pub f: FlagsRegister,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub h: u8,
+    pub l: u8,
 }
 
 impl Default for Registers {
     fn default() -> Self {
         Self {
             a: 0x01,
-            f: 0xB0,
+            f: 0xB0.into(),
             b: 0x00,
             c: 0x13,
             d: 0x00,
